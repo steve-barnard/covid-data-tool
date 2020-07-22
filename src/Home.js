28da01faecb516a3467766b32b4e909673ca11d6
Steve Barnard <steven.mcfarland.barnard@gmail.com>
2020-07-22 09:45:30 -0400

    updated Home screen text

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,8 @@ export const Home = () =>
     (
         <div>
            <h2>Home</h2>
-           <p>Test tesst 12345 abc</p>
-           <p>abc ABC tesst 12345 abc</p> 
+           <p>The purpose of this site, which is currently a work in progress, is to provide information on Covid-19. </p>
+           <p>The information will primarily focus on using machine learning and other biostatistical tools to allow for</p>
+	   <p>the comparrison and analysis of different viral sequences. </p> 
         </div>
     )
